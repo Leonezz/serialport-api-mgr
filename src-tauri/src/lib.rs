303f1d7cb78bb3ae64408279cd23c1f87f4c757b
zhuwenq <zhuwenqa@outlook.com>
2024-08-27 21:57:04 +0800

    add devtools as dev logging tool

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,69 +6,64 @@ mod error;
 mod serial_mgr;
 mod util;
 
-use tauri::{ self };
-use tauri_plugin_log::{ self, Target, TargetKind, TimezoneStrategy };
 use crate::bridge::{
-    hello,
-    get_all_port_info,
-    open_port,
-    close_port,
-    write_port,
-    test_async,
-    write_dtr,
-    write_rts,
+    close_port, get_all_port_info, hello, open_port, test_async, write_dtr, write_port, write_rts,
 };
+use tauri::{self};
+use tauri_plugin_devtools::{self};
+use tauri_plugin_log::{self, Target, TargetKind, TimezoneStrategy};
 pub fn run() {
-    tauri::Builder
-        ::default()
-        .invoke_handler(
-            tauri::generate_handler![
-                hello,
-                get_all_port_info,
-                open_port,
-                close_port,
-                write_port,
-                test_async,
-                write_dtr,
-                write_rts
-            ]
-        )
-        .plugin(
-            tauri_plugin_log::Builder
-                ::new()
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(debug_assertions)]
+    {
+        let dev_tools = tauri_plugin_devtools::init();
+        builder = builder.plugin(dev_tools);
+    }
+    #[cfg(not(debug_assertions))]
+    {
+        builder = builder.plugin(
+            tauri_plugin_log::Builder::new()
                 .clear_targets()
                 .target(Target::new(TargetKind::Stdout))
                 .target(Target::new(TargetKind::Webview))
-                .target(
-                    Target::new(TargetKind::Folder {
-                        path: "logs".into(),
-                        file_name: Some("app.log".to_string()),
-                    })
-                )
+                .target(Target::new(TargetKind::Folder {
+                    path: "logs".into(),
+                    file_name: Some("app.log".to_string()),
+                }))
                 .format(|out, message, record| {
-                    out.finish(
-                        format_args!(
-                            "[{}] [{}] [{}:{}] [{:?}] {}",
-                            record.level(),
-                            record.target(),
-                            record.file().unwrap_or("unknown"),
-                            record.line().unwrap_or(0),
-                            std::thread::current().id(),
-                            message
-                        )
-                    )
+                    out.finish(format_args!(
+                        "[{}] [{}] [{}:{}] [{:?}] {}",
+                        record.level(),
+                        record.target(),
+                        record.file().unwrap_or("unknown"),
+                        record.line().unwrap_or(0),
+                        std::thread::current().id(),
+                        message
+                    ))
                 })
                 .timezone_strategy(TimezoneStrategy::UseLocal)
-                .build()
+                .build(),
         )
+    }
+
+    builder
+        .invoke_handler(tauri::generate_handler![
+            hello,
+            get_all_port_info,
+            open_port,
+            close_port,
+            write_port,
+            test_async,
+            write_dtr,
+            write_rts
+        ])
         .build(tauri::generate_context!())
         .expect("error whiling running tauri application")
-        .run(|_app, event| {
-            match event {
-                tauri::RunEvent::Ready => {
-                    log::info!("App is running!");
-                }
-                _ => {}
+        .run(|_app, event| match event {
+            tauri::RunEvent::Ready => {
+                log::info!("App is running!");
             }
+            _ => {}
         });
 }
